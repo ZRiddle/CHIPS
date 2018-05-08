91d178eeb682c76a7e0f76c94cb68b68286833e3
Zach-Riddle <zach.riddle@progleasing.com>
2018-05-08 10:46:43 -0600

    sample setup for deploying titanic RandomForest model

---
+++ models/titanic_sample/cloud_storage.py
@@ -0,0 +1,31 @@
+import os
+import logging
+import pickle
+
+from google.cloud import storage
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+
+
+class FileStorage(object):
+    """
+    directory: project_hash string
+    filename: ['data', 'model', 'column', 'column_ohe']
+    """
+
+    def __init__(self):
+        gcs = storage.Client()
+        self.bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    def save_pickle(self, directory, filename, obj):
+        dir = directory + '/' + filename + '.pkl'
+        logging.info("Saving pkl: {}".format(dir))
+        blob = self.bucket.blob(dir)
+        blob.upload_from_file(pickle.dump(obj))
+        return
+
+    def load_pickle(self, directory, filename):
+        dir = directory + '/' + filename + '.pkl'
+        logging.info("Loading pkl: {}".format(dir))
+        blob = self.bucket.blob(dir)
+        obj = pickle.load(blob.download_as_file())
+        return obj
